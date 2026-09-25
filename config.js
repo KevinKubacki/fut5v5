5d52f1e9d0cf15a8a0673a73fe0a386a0ae39563
KevinKubacki <kevin.kubacki@gmail.com>
2026-09-25 14:20:19 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/* FUT 5V5 : réglage unique à faire après le déploiement du script Google.
+   Colle entre les guillemets l'URL de l'application Web (elle se termine par /exec). */
+window.FUT5V5_CONFIG = {
+  apiUrl: "COLLER_ICI_L_URL_DU_SCRIPT_GOOGLE"
+};
