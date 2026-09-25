959880368167de94ba17b2d1daf00b97cf232726
KevinKubacki <kevin.kubacki@gmail.com>
2026-09-25 14:24:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* FUT 5V5 : réglage unique à faire après le déploiement du script Google.
    Colle entre les guillemets l'URL de l'application Web (elle se termine par /exec). */
 window.FUT5V5_CONFIG = {
-  apiUrl: "COLLER_ICI_L_URL_DU_SCRIPT_GOOGLE"
+  apiUrl: "https://script.google.com/macros/s/AKfycbxCISnwKmq6TCxWMgahOiH7EbxT7KoR9yvMjfNIswbZyvGeSunlb_ltTlm9_TVYlB3Eyw/exec"
 };
